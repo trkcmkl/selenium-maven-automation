ded12767607e1ae291196b31ab1d1b608b3bbbc1
trkcmkl <trkcmkl@gmail.com>
2018-06-14 05:19:00 -0400

    3.0

--- src/test/java/com/dice/Dice_job_search.java
+++ src/test/java/com/dice/Dice_job_search.java
@@ -1,7 +1,13 @@
 package com.dice;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.management.RuntimeErrorException;
@@ -10,37 +16,55 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Dice_job_search {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver",
-				"/Users/Tarik/Documents/ selenium dependencies/drivers/chromedriver");
-
+		WebDriverManager.chromedriver().setup();
 		WebDriver dr = new ChromeDriver();
+
+		List<String> jobs = new ArrayList<>();
+		FileReader file = new FileReader("jobsearch.txt");
+		BufferedReader bf = new BufferedReader(file);
+
+		String jobss = "";
+		while ((jobss = bf.readLine()) != null) {
+			jobs.add(jobss);
+		}
+
 		dr.switchTo();
 		dr.manage().window().fullscreen();
 		dr.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		dr.get("http://dice.com");
-		dr.findElement(By.id("search-field-location")).clear();
-		dr.findElement(By.id("search-field-location")).sendKeys("20707");
-		dr.findElement(By.id("search-field-keyword")).clear();
-		dr.findElement(By.id("search-field-keyword")).sendKeys("find tech jobs");
-		
-		
-		dr.findElement(By.id("findTechJobs")).click();
-		if (dr.getTitle().contains("find tech jobs jobs in 20707") ) {
-			System.out.println("working well");
-		} else {
-			System.out.println("failed");
-			throw new RuntimeException("dice page didnt loaded successfully");
+		for (int i = 0; i < 2; i++) {
+			dr.findElement(By.id("search-field-location")).clear();
+			dr.findElement(By.id("search-field-location")).sendKeys("20707");
+			dr.findElement(By.id("search-field-keyword")).clear();
+			dr.findElement(By.id("search-field-keyword")).sendKeys(jobs.get(i));
+
+			dr.findElement(By.id("findTechJobs")).click();
+			if (dr.getTitle().contains("jobs")) {
+				System.out.println("working well");
+			} else {
+				System.out.println("failed");
+				throw new RuntimeException("dice page didnt loaded successfully");
+			}
+			String a = dr.findElement(By.id("posiCountId")).getText();
+			
+			System.out.println("Job Title : "+jobs.get(i)+ " available positions : "+ a);
+			
+			System.out.println("test complete" + LocalDateTime.now());
+
+//			String count = dr.findElement(By.xpath("//*[@id=\"posiCountMobileId\"]")).toString();
+//			System.out.println(count);
+			Thread.sleep(1000);
+			dr.navigate().back();
 		}
-		String a=dr.findElement(By.id("posiCountMobileId")).getText();
-		System.out.println(a);
-		
-		System.out.println("test complete"+ LocalDateTime.now());
-		Thread.sleep(2000);
-		dr.close();
+		bf.close();
+//		dr.close();
+		dr.quit();
 	}
 }
